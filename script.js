3d15fc6987cfc167b43f9dfe1c340a0fc4fec182
HH <hhoefele@gmail.com>
2017-01-04 19:36:03 -0500

    added slideshow example

--- script.js
+++ script.js
@@ -47,9 +47,8 @@
    var canvas = document.getElementById('myCanvasTag6');  
    var context = canvas.getContext('2d');  
    var imgObj = new Image();  
-
    imgObj.onload = function() {  
-   context.drawImage(imgObj,10,10);  
+   context.drawImage(imgObj,25,25);  
    };  
    imgObj.src = 'http://www.w3resource.com/images/logo.png'; 
 
@@ -67,25 +66,93 @@
    context2.fillText("Welcome!",100,100);//sets text to be rendered 
 
 
-// HTML5 Canvas Text, Awesome Fonts Icon, Example #8
-// http://stackoverflow.com/questions/35570801/how-to-draw-font-awesome-icons-onto-html-canvas
-// http://www.cheezycode.com/2015/07/font-awesome-with-html5-canvas.html
-   var iconcanvas = document.getElementById('myCanvasTag8');  
-   var context3 = iconcanvas.getContext('2d');  
-   context3.shadowBlur=20;
-   context3.shadowColor="black";
-   context3.font = '60pt FontAwesome'; //sets font style, size and type
-   context3.fillText("\uf099", 10, 100); //sets text to be rendered  
-   context3.fillText("\uf21c",80,200);
+// HTML5 Canvas Text, Empty Box, Example #8
+   var emptycanvas = document.getElementById('myCanvasTag9');  
+   var context4 = emptycanvas.getContext('2d');
+
 
 // HTML5 Canvas Text, HTML Ampersand, Example #9
+// http://stackoverflow.com/questions/35570801/how-to-draw-font-awesome-icons-onto-html-canvas
+// http://www.cheezycode.com/2015/07/font-awesome-with-html5-canvas.html
    var ampersandcanvas = document.getElementById('myCanvasTag9');  
    var context4 = ampersandcanvas.getContext('2d');  
    context4.font = 'bold 80pt Arial';//sets font style, size and type  
    context4.fillStyle = 'orange';//sets font color  
    context4.textAlign="center";  
    context4.fillText("&",100,100);//sets text to be rendered 
+   
+// HTML5 Canvas Text, Basic Animation with Canvas example, Example #10
 
+	var canvas3;    
+	var ctx;  
+	var x = 400;  
+	var y = 300;  
+	var dx = 2;  
+	var dy = 4;  
+	var width = 400;  
+	var height = 300;   
+  
+	function circle(x,y,r) {  
+	  ctx.beginPath();  
+	  ctx.arc(x, y, r, 0, Math.PI*2, true);  
+	  ctx.fill();  
+	}  
+  
+	function rect(x,y,w,h) {  
+	  ctx.beginPath();  
+	  ctx.rect(x,y,w,h);  
+	  ctx.closePath();  
+	  ctx.fill();  
+	}  
+  
+   
+	function reset() {  
+	  ctx.clearRect(0, 0, width, height);  
+	}  
+  
+	function init() {  
+	  canvas3 = document.getElementById("myCanvasTag10");  
+	  ctx = myCanvasTag10.getContext("2d");  
+	  return setInterval(animate, 10);  
+	}  
+  
+  
+	function animate() {  
+	  reset();  
+	  ctx.fillStyle = "#FAF7F8";  
+	  rect(0,0,width,height);  
+	  ctx.fillStyle = "#444444";  
+	  circle(x, y, 10);  
+  
+	  if (x + dx > width || x + dx < 0)  
+		dx = -dx;  
+	  if (y + dy > height || y + dy < 0)  
+		dy = -dy;  
+  
+	  x += dx;  
+	  y += dy;  
+	}  
+  
+	init();  
+	
+// HTML5 Container #1, Slideshow
+	
+	
+	var slideIndex = 0;
+	carousel();
+
+	function carousel() {
+		var i;
+		var x2 = document.getElementsByClassName("mySlides");
+		for (i = 0; i < x2.length; i++) {
+		  x2[i].style.display = "none"; 
+		}
+		slideIndex++;
+		if (slideIndex > x2.length) {slideIndex = 1} 
+		x2[slideIndex-1].style.display = "block"; 
+		setTimeout(carousel, 1000); 
+		}
+		
 
 
 }
